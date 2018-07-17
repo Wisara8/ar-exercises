5743d7203f3f572bd77f52eeffb905e202548ba1
Wisara8 <srandall845@gmail.com>
2018-07-17 20:51:48 +0000

    E5 complete

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,40 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+surrey = Store.new
+
+surrey.name = "Surrey"
+surrey.annual_revenue = 224000
+surrey.mens_apparel = false
+surrey.womens_apparel = true
+surrey.save
+
+whistler = Store.new
+
+whistler.name = "Whistler"
+whistler.annual_revenue = 1900000
+whistler.mens_apparel = true
+whistler.womens_apparel = false
+whistler.save
+
+yaletown = Store.new
+
+yaletown.name = "Yaletown"
+yaletown.annual_revenue = 430000
+yaletown.mens_apparel = true
+yaletown.womens_apparel = true
+yaletown.save
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |shop|
+  puts "#{shop.name} #{shop.annual_revenue}" 
+end
+
+# @womens_stores = Store.where(womens_apparel: true, annual_revenue: < 1000000) this does not work
+@womens_stores = Store.where("womens_apparel = ? AND annual_revenue < ?", true, 1000000)
+
+
+@womens_stores.each do |shop|
+  puts "#{shop.name} #{shop.annual_revenue}"
+end
--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,13 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@sum = Store.sum(:annual_revenue)
+puts "#{@sum}"
+
+@average = Store.average(:annual_revenue)
+puts "#{@average}"
+
+@selection = Store.where("annual_revenue > ?", 1000000).count
+puts "#{@selection}"
+
+
